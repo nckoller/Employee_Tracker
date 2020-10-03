4189fa7adb7533185f8298df26ec6771ca99ceb0
Nina Koller <nckoller@gmail.com>
2020-10-03 00:12:45 -0500

    added createNewRole and insertNewRole functions, added console.table to package.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const connection = require("./database/connection");
 
+// Adds a new department to the database
 function insertNewDept() {
   inquirer
     .prompt([
@@ -25,4 +26,93 @@ function insertNewDept() {
     });
 }
 
-insertNewDept();
+// insertNewDept();
+
+// Add new role
+function createNewRole() {
+  connection.query("SELECT * FROM department", function (err, results) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "What role would you like to add?",
+        },
+        {
+          name: "salary",
+          type: "number",
+          message: "what is the salary?",
+        },
+        {
+          name: "department",
+          type: "rawlist",
+          choices: function () {
+            let departmentArr = [];
+            for (var i = 0; i < results.length; i++) {
+              departmentArr.push(results[i].name);
+            }
+            return departmentArr;
+          },
+          message: "Which department will this role belong to?",
+        },
+      ])
+      .then(function (answer) {
+        // Add the department ID to the role
+        let idFound = false;
+        let i = 0;
+        while (idFound === false) {
+          if (results[i].name === answer.department) {
+            answer.departmentId = results[i].id;
+            idFound = true;
+            insertNewRole(answer);
+          }
+          i++;
+        }
+      });
+
+    //
+    //     {
+    //         name: "salary",
+    //         type: "number",
+    //         message: "What is the salary?"
+    //     },
+
+    // ])
+    // .then(function (answer) {
+    //     connection.query(
+    //         "INSERT INTO role SET ?",
+    //         {
+    //             name:
+    //         }
+    //     )
+    // })
+  });
+}
+
+function insertNewRole(answer) {
+  connection.query(
+    "INSERT INTO role SET ?",
+    {
+      title: answer.title,
+      salary: answer.salary,
+      departmentId: answer.departmentId,
+    },
+    function (err) {
+      if (err) throw err;
+      console.log("The new role was created successfully.");
+    }
+  );
+}
+createNewRole();
+// Add new employee
+
+// View all departments
+function viewDepartments() {
+  connection.query;
+}
+// View all roles
+
+// View all employees
+
+// Update employee roles
